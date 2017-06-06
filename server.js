edb4bcc314b440e9adff60b3e11d50e885fc48cd
JirLu <s1365789@gmail.com>
2017-06-06 23:46:11 +0800

    ver4

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express'),
 	bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://aabb:aadd@ds063140.mongolab.com:63140/ddatebase');
+mongoose.connect('mongodb://localhost/Tododb');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
